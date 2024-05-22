a7f6b9ea2ef8fa7a30223f887528be318e6b362a
Anemany <marija.nanojana@gmail.com>
2024-05-22 15:02:55 +0300

    izveidoju jaunu varoni, teksta ievades laukus, cepures

---
+++ Assets/Script/vardaparadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vardaparadisana : MonoBehaviour
+{
+    void Update()
+    {
+    }
+    public string teksts;
+    //Lauks kurā ierakstīs tekstu
+    public GameObject ievadesLauks;
+    //Kur attēlos tekstu
+    public GameObject tekstaAttelosana;
+    public void uzglabaTekstu()
+    {
+        //No komponentes Ievades Lauks1 iegūst pakārtoto Text komponenti un
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper() + "!";
+    }
+}
